9cf2e7cbdb9809fa8c7da5402bc167bff38c9931
Maarten Jorens <Maarten.Jorens@infosupport.com>
2021-04-21 22:03:04 +0200

    Added Server Streaming of BeerReply

--- Beer-gRPC/BeerClient/Program.cs
+++ Beer-gRPC/BeerClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using BeerService;
+using Grpc.Core;
 using Grpc.Net.Client;
 using System.Threading.Tasks;
 
@@ -9,14 +10,14 @@ namespace BeerClient
         static async Task Main(string[] args)
         {
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-
-            await GetBeer(channel);
-        }
-
-        private static async Task GetBeer(GrpcChannel channel)
-        {
             var client = new Beers.BeersClient(channel);
 
+            await GetBeer(client);
+            await GetBeers(client);
+        }
+
+        private static async Task GetBeer(Beers.BeersClient client)
+        {
             var request = new BeerRequest()
             {
                 Name = "Duvel"
@@ -25,6 +26,19 @@ namespace BeerClient
             var reply = await client.GetBeerAsync(request);
 
             System.Console.WriteLine(reply);
+        }
+
+        private static async Task GetBeers(Beers.BeersClient client)
+        {
+            using (var call = client.GetBeers(new Google.Protobuf.WellKnownTypes.Empty()))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    BeerReply beerReply = call.ResponseStream.Current;
+                    System.Console.WriteLine(beerReply);
+                }
+            }
+
         }
     }
 }
--- Beer-gRPC/BeerService/Services/BeerService.cs
+++ Beer-gRPC/BeerService/Services/BeerService.cs
@@ -2,7 +2,6 @@ using Grpc.Core;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace BeerService
@@ -18,14 +17,68 @@ namespace BeerService
         public override Task<BeerReply> GetBeer(BeerRequest request, ServerCallContext context)
         {
             _logger.LogInformation("Requesting beer with name: {name}", request.Name);
+            var bestBeforeDate = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(DateTime.UtcNow.AddDays(30));
+
             return Task.FromResult(new BeerReply
             {
                 Id = 1,
                 Name = "Duvel",
                 AlcoholPercentage = 8.5,
                 EBC = 5.7,
-                EBU = 32
+                EBU = 32,
+                BestBefore = bestBeforeDate,
+                BeerType = BeerType.BelgianBlond,
+                Hops =
+                {
+                    "citra",
+                    "admiral"
+                }
             });
+        }
+
+        public override async Task GetBeers(
+            Google.Protobuf.WellKnownTypes.Empty request, 
+            IServerStreamWriter<BeerReply> responseStream, 
+            ServerCallContext context)
+        {
+            var beers = new List<BeerReply>()
+            {
+                new BeerReply
+                {
+                    Id = 1,
+                    Name = "Duvel",
+                    AlcoholPercentage = 8.5,
+                    EBC = 5.7,
+                    EBU = 32,
+                    BestBefore = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(DateTime.UtcNow.AddDays(30)),
+                    BeerType = BeerType.BelgianBlond,
+                    Hops =
+                    {
+                        "citra",
+                        "admiral"
+                    }
+                },
+                new BeerReply
+                {
+                    Id = 2,
+                    Name = "Westmalle Tripel",
+                    AlcoholPercentage = 9,
+                    EBC = 5.7,
+                    EBU = 32,
+                    BestBefore = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(DateTime.UtcNow.AddDays(90)),
+                    BeerType = BeerType.Trappist,
+                    Hops =
+                    {
+                        "admiral"
+                    }
+                }
+            };
+
+            foreach (var beer in beers)
+            {
+                await responseStream.WriteAsync(beer);
+                await Task.Delay(5000);
+            }
         }
     }
 }
